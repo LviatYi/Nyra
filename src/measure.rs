0aaf28c509d3ef2eb3ad380937fe4fb990398880
Lviat Yi <LviatYi@foxmail.com>
2026-03-17 14:57:53 +0800

    fix: update logging to use more descriptive elapsed time field in measure function

--- src/measure.rs
+++ src/measure.rs
@@ -5,7 +5,7 @@ pub fn measure<R>(label: &str, f: impl FnOnce() -> R) -> R {
     let result = f();
     let elapsed = start.elapsed();
 
-    tracing::info!(target = label, elapsed = elapsed.as_secs_f64(), "{}", MEASURE_TRACING_TITLE);
+    tracing::info!(target = label, elapsed_sec = elapsed.as_secs_f64(), "{}", MEASURE_TRACING_TITLE);
 
     result
 }
